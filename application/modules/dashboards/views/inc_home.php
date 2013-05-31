14c671a302efa28e79c6f38fe184911fb6134cd5
unisexx <unisexx@hotmail.com>
2013-05-31 07:26:44 +0700

    ...

--- application/modules/dashboards/views/inc_home.php
+++
@@ -1,3 +0,0 @@
-<div class="title_news" style="text-align:center; margin:10px 0 20px 0;">
-<img src="themes/bie/images/count.png" width="18" height="18" /> ผู้เข้าชม   <?=$allTimeSummery['ga:visits']?> คน
-<img src="themes/bie/images/page.png" width="18" height="18" /> การแสดงหน้าเว็บ  <?=$allTimeSummery['ga:pageviews']?> ครั้ง </div>
