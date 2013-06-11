c4a0d06332d78db06877c730fab4daa2a295cd45
unisexx <unisexx@hotmail.com>
2013-06-11 15:13:15 +0700

    ...

--- application/libraries/Public_Controller.php
+++ application/libraries/Public_Controller.php
@@ -15,6 +15,10 @@ class Public_Controller extends Master_Controller
         
         // Set js
         $this->template->append_metadata(js_notify());
+		
+		// Set Keywords , Description
+        $this->template->append_metadata( meta('description','ประยงด์ปาร์เก้ รับทำ ไม้พื้นปาร์เก้ ไม้พื้นลามิเนต รับเมหาและตกแต่ง งานก่อสร้าง ปูพื้นปาร์เก้ และซ่อมแซมส่วนภายในบ้านและอาคาร ติดตั้ง อุปกรณ์ ครุภัณฑ์ ภายในบ้านและตกแต่ง ราคาถูก'));
+        $this->template->append_metadata( meta('keywords','ปาร์เก้,ทำปาร์เก้,ไม้ปาร์เก้,พื้นปาร์เก้,ไม้พื้นปาร์เก้,ปูพื้นปาร์เก้,ประยงด์ปาร์เก้,ไม้พื้นลามิเนต,รับเหมาและตกแต่ง,งานก่อสร้าง,ราคาถูก'));
 	}
 }
 ?>
